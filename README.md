4edb8332c001c98c2e2aa5eeffeeac7864f9fc3a
kritebh <kritebhlaganbibhakar@gmail.com>
2021-05-01 19:13:47 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## Love Calculator 💖
+
+> This is a simple API based project in Django.
+
+`Homepage`
+
+![HomePage](homepage.png "HomePage")
+
+`Result Page`
+
+![Result Page](result.png)
+
+> API Link - https://rapidapi.com/ajith/api/love-calculator
+
+> I have configured three images based on match percentage which you can change according to your Preference.
---
+++ core/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+import requests
+# Create your views here.
+
+def home(request):
+    try:
+        if request.method=='POST':
+            f = request.POST.get('fname',False)
+            s = request.POST.get('sname',False)
+            res = calculator(f,s)
+            context ={
+            'fname':res['fname'],
+            'sname':res['sname'],
+            'percent':int(res['percentage']),
+            'result':res['result'],
+            }
+            return render(request,'core/result.html',context={'data':context})
+        return render(request,'core/home.html')
+    except:
+        return render(request,'core/error.html')
+    
+# def test(request):
+#     return render(request,'core/error.html')
+
+def calculator(f,s):
+    url = "https://love-calculator.p.rapidapi.com/getPercentage"
+
+    querystring = {"fname":f"{f}","sname":f"{s}"}
+
+    headers = {
+        'x-rapidapi-key': "7f119531abmsh0fdb7cd00e5a13bp152b0cjsnd4b793db18d7",
+        'x-rapidapi-host': "love-calculator.p.rapidapi.com"}
+
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    return response
